e8366d520addfd72c75dfd774134a867d3de6b0d
Jorge Barata <jorge.barata.gonzalez@gmail.com>
2023-05-11 17:27:27 +0200

    Comment vite.config.ts renaming fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [svelte()],
   base: process.env.BASE_URL,
   esbuild: {
-    keepNames: true  // Prevent renaming of inner functions https://github.com/vitejs/vite/issues/11136
+    keepNames: true  // Don't rename inner functions, e.g. client.moves.*
   }
 })
