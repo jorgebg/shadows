91fac4d81a97128ea7984fef7467d96ace585282
Jorge Barata <jorge.barata.gonzalez@gmail.com>
2024-05-26 16:06:14 +0200

    local webfonts

--- src/App.svelte
+++ src/App.svelte
@@ -4,21 +4,5 @@
   import "./app.scss";
 </script>
 
-<svelte:head>
-  <!-- Hint where we get fonts from. -->
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-
-  <!-- Material Symbols, Roboto, and Roboto Mono fonts -->
-  <link
-    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:FILL@1"
-    rel="stylesheet"
-  />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital@0;1&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-    rel="stylesheet"
-  />
-</svelte:head>
-
 <GameApp config={getGameAppConfig()} />
 <!-- <GameApp config={getExampleConfig()} /> -->
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
+import { webfontDownload } from "vite-plugin-webfont-dl";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), tsconfigPaths()],
+  plugins: [svelte(), tsconfigPaths(), webfontDownload()],
   base: process.env.BASE_URL,
   esbuild: {
     keepNames: true, // Don't rename inner functions, e.g. client.moves.*
